6453870599535f19dcaae4e91d9b013d839745d5
MOHAMED AMINE LAROUCI <117996634+Amine-Minato@users.noreply.github.com>
2023-05-27 19:51:56 +0200

    main.py

--- main.py
+++ main.py
@@ -1,23 +1,18 @@
 import re
 import pickle
 from string import punctuation
-from pyngrok import ngrok
 import uvicorn
-from starlette.middleware.cors import CORSMiddleware
-from fastapi import FastAPI
-import tensorflow_hub as hub
 import nltk
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
+import tensorflow_hub as hub
+from pyngrok import ngrok
+from starlette.middleware.cors import CORSMiddleware
+from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.on_event("startup")
-async def startup_event():
-    await download_nltk_resources()
-    import your_module_using_nltk
-
-async def download_nltk_resources():
+def download_nltk_resources():
     nltk.download("stopwords")
     nltk.download("wordnet")
 
@@ -28,33 +23,8 @@ def initialize_nltk_resources():
 
 stop_words, lemmatizer = initialize_nltk_resources()
 
-embed = hub.load("https://tfhub.dev/google/universal-sentence-encoder/4")
-
-ngrok_tunnel = ngrok.connect(8000)
-print("Public URL:", ngrok_tunnel.public_url)
-
-app = FastAPI(
-    title="Suggestion Tags API",
-    description="A simple API that uses an NLP model to predict tag suggestions",
-    version="0.1",
-)
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-with open('saved_model.pkl', 'rb') as f:
-    loaded_model = pickle.load(f)
-
-with open('mlb.pkl', 'rb') as f:
-    mlb = pickle.load(f)
-
-def feature_USE_fct(sentences, b_size=1):
-    batch_size = b_size
+def feature_USE_fct(sentences):
+    embed = hub.load("https://tfhub.dev/google/universal-sentence-encoder/4")
     features_list = []
     feat = embed(sentences)
     return feat
@@ -63,7 +33,7 @@ def text_cleaning(text, remove_stop_words=True, lemmatize_words=False):
     text = re.sub(r"[^A-Za-z0-9]", " ", text)
     text = re.sub(r"'s", " ", text)
     text = re.sub(r"http\S+", " link ", text)
-    text = re.sub(r"\d+(?:\.\d+)?\s+", "", text)  # remove numbers
+    text = re.sub(r"\b\d+(?:\.\d+)?\s+", "", text)  # remove numbers
 
     text = "".join([c for c in text if c not in punctuation])
 
@@ -79,9 +49,23 @@ def text_cleaning(text, remove_stop_words=True, lemmatize_words=False):
 
     return text
 
-@app.post("/predict-tags")
+loaded_model = None
+mlb = None
+
+@app.on_event("startup")
+async def startup_event():
+    download_nltk_resources()
+    global loaded_model, mlb
+    with open('saved_model.pkl', 'rb') as f:
+        loaded_model = pickle.load(f)
+
+    with open('mlb.pkl', 'rb') as f:
+        mlb = pickle.load(f)
+
+@app.get("/predict-tags")
 def predict_tags(tags: str):
     cleaned_tags = text_cleaning(tags)
+    
     text_vector_use = feature_USE_fct([cleaned_tags])
 
     prediction = loaded_model.predict(text_vector_use)
